4f0132e3db7e482b30e305bfdbf0566eed45f0cf
milosmikovic <losmicarina@gmail.com>
2023-06-19 21:02:23 +0200

    wmm bez look ahead

--- SCS.cpp
+++ SCS.cpp
@@ -125,6 +125,28 @@ class SCS
             fResults << "------------------" << std::endl;
         }
 
+        void SCS_WMM_Beam_Search_Algorithm()
+        {
+            printAlphabetAndSequences();
+            fResults << "------------------" << std::endl;
+            fResults << "SCS_WMM_Beam_Search_Algorithm" << std::endl;
+            time_t start, end;
+            time(&start);
+
+            setScsMaxDepth();
+            shortestCommonSupersequenceLen = maxDepth;
+
+            WMM_Beam_Search();
+
+            fResults << "-WMM Beam Search solution-" << std::endl;
+            fResults << "SCS : " + shortestCommonSupersequence << std::endl;
+            fResults << "SCS length : " << shortestCommonSupersequenceLen << std::endl;
+
+            time(&end);
+            measureTime(start, end);
+            fResults << "------------------" << std::endl;
+        }
+
         static bool sortRev1(const std::pair<std::size_t, std::string> &a,  const std::pair<std::size_t, std::string> &b) 
         { 
             return (a.first > b.first); 
@@ -803,6 +825,133 @@ class SCS
             return std::pair<std::string, std::size_t>(retSls, weight);
         }
 
+        void WMM_Beam_Search()
+        {
+            // BEGIN : algorithm initialization
+            /* 
+                solutions are pairs where first is solution string, second is pair where second.first is rank of solution and second.second is vector of positions is setOfStrings
+            */
+            std::vector<std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>>> *sls1 
+                                                = new std::vector<std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>>>();
+
+            std::vector<std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>>> *sls2 =
+                                                 new std::vector<std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>>>();
+
+            bool isSuperSequenceFound = false;
+
+            std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>> initialSolution
+                        (        
+                        std::string(1,alphabet[0]),
+                        std::pair<size_t, std::vector<std::size_t>>(0,std::vector<std::size_t>(setOfStringsSize,0))
+                        );
+            std::vector<std::pair<char, std::size_t>> lr = WMM(initialSolution);
+
+            for(auto &c : alphabet)
+            {
+                std::size_t weigthOfC;
+                for(auto &l : lr)
+                {
+                    if(c == l.first)
+                    {
+                        weigthOfC = l.second;
+                    }
+                }
+
+                std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>> tmpSls
+                    (
+                        std::string(1,c), std::pair<size_t, std::vector<std::size_t>>(weigthOfC,std::vector<std::size_t>(setOfStringsSize,0))
+                    );
+                sls1->emplace_back(tmpSls);
+            }
+
+            sls1 = takeBetaSequances(sls1,isSuperSequenceFound);
+
+            // END : algorithm initialization
+            // fResults << "***START***" << std::endl;
+            // main loop of Beam Search algorithms
+            while(!isSuperSequenceFound)
+            {
+                for(auto &s : *sls1)
+                {
+                    // calculation of ranks of partial solutions
+                    // fResults << "Ranks for seq:" << s.first << std::endl; 
+                    std::vector<std::pair<char, std::size_t>> letterRanks = WMM(s);
+                    /*
+                     if all the ranks of the letters are 0 then there is a solution, actually if the rank of the first
+                     letter is 0 it means that the others are also because they are sorted
+                    */
+                    if(letterRanks[0].second == 0)
+                    {
+                        std::string solution = s.first;
+                        std::string postRes("");
+                        for(int i = 0;i < setOfStringsSize;++i)
+                        {
+                            // inside the loop we remove the remaining letters from setOfStrings but without repetition
+                            std::size_t posInSqs = s.second.second[i];
+                            while(posInSqs < setOfStringsVec[i].size())
+                            {
+                                char postChar = setOfStringsVec[i][posInSqs];
+                                if(std::find(postRes.begin(),postRes.end(),postChar) == postRes.end())
+                                {
+                                    postRes += postChar;
+                                }
+
+                                ++posInSqs;
+                                // if we find a supersequence we terminate the loop execution earlyer
+                                if(isCommonSupersequence(solution + postRes))
+                                {
+                                    solution += postRes;
+                                    shortestCommonSupersequence = solution;
+                                    shortestCommonSupersequenceLen = solution.length();
+                                    isSuperSequenceFound = true;
+                                    break;
+                                }
+                            }
+                            if(isSuperSequenceFound)
+                            {
+                                break;
+                            }
+                        }
+                    }
+
+                    //we add characters from the alphabet to the fixed partial solution and assign a new rank based on the ranks of the letters
+                    for(auto &c : alphabet)
+                    {
+                        std::size_t weigthOfC;
+                        for(auto &l : letterRanks)
+                        {
+                            if(c == l.first)
+                            {
+                                weigthOfC = l.second;
+                            }
+                        }
+                        std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>> newSLS
+                        (
+                            std::string(s.first + c), std::pair<size_t, std::vector<std::size_t>>(s.second.first + weigthOfC, s.second.second)
+                        );
+                        sls2->emplace_back(newSLS);
+                    }
+                }
+                
+                // we choose the best beam size solutions from the newly created partial solutions
+                delete sls1;
+                sls1 = takeBetaSequances(sls2,isSuperSequenceFound);
+                sls2 = new std::vector<std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>>>();       
+            }
+            
+
+            if(sls1)
+            {
+                delete sls1;
+            }
+
+            if(sls2)
+            {
+                delete sls2;
+            }
+
+        }
+
         void LAWMM_Beam_Search()
         {
             // BEGIN : algorithm initialization
@@ -1086,6 +1235,41 @@ class SCS
             return letterRanks;
         }
 
+        std::vector<std::pair<char, std::size_t>> WMM(std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>> &s)
+        {
+            std::vector<std::pair<char, std::size_t>> letterRanks;
+            for(auto &c : alphabet)
+            {
+                letterRanks.emplace_back(c,0);
+            }
+
+            for(int i = 0;i < setOfStringsVec.size();++i)
+            {
+                std::vector<std::size_t> tmpPositions = s.second.second;
+                std::size_t lastIndex = setOfStringsVec[i].size() - 1;
+                if(tmpPositions[i] <= lastIndex)
+                {
+                    for(auto &lr : letterRanks)
+                    {
+                        if(lr.first == setOfStringsVec[i][tmpPositions[i]])
+                        {
+                            lr.second += (lastIndex - tmpPositions[i]);
+                        }
+                    }
+                }
+            }            
+
+
+            // we mix the elements so that 'a' would not always be selected if say a,c,t all have the same value
+            auto rng = std::default_random_engine();
+            rng.seed(std::chrono::system_clock::now().time_since_epoch().count());
+            std::shuffle(letterRanks.begin(), letterRanks.end(), rng);
+
+            std::sort(letterRanks.begin(), letterRanks.end(), sortRev2);
+            
+            return letterRanks;
+        }
+
         // updating positions for partial solution: We move in each sequence in setOfStrings for the last added letter in partial solution
         void updatePositions(std::pair<std::string, std::pair<size_t, std::vector<std::size_t>>> &s)
         {
@@ -1137,6 +1321,8 @@ class SCS
 
 
 
+
+
         // the sequence that represents the solution to the problem and its size 
         std::string shortestCommonSupersequence = std::string("");
         std::size_t shortestCommonSupersequenceLen = 0;
@@ -1179,6 +1365,8 @@ int main(int argc, char **argv)
     alg.SCS_MM_Beam_Search_Algorithm();
     // alg.LAWMMAlgorithm();
     alg.SCS_LAWMM_Beam_Search_Algorithm();
+
+    alg.SCS_WMM_Beam_Search_Algorithm();
 
     return EXIT_SUCCESS;
 
