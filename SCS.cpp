c3bcccd40569bdb7176762ab182c12aac4a28a4a
milosmikovic <losmicarina@gmail.com>
2023-07-13 01:36:48 +0200

    ispravke glava 3-4

--- SCS.cpp
+++ SCS.cpp
@@ -1337,7 +1337,7 @@ class SCS
         std::size_t setOfStringsSize;
 
         // Beam size paramether of Beam Search algorithm
-        const std::size_t beamSize = 100;
+        const std::size_t beamSize = 16;
 
         // LAWMM lookAhead parameter
         std::size_t lookAhead = 1;
